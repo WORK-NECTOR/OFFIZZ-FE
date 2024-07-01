c90121b05394c9695e852e46f87d5a983962b2d8
leehyewon0531 <ifpffk@naver.com>
2024-07-02 00:05:34 +0900

    OFZ-7 style: TitleDesc 기본 스타일 작업

---
+++ src/components/TitleDesc/TitleDesc.styled.ts
@@ -0,0 +1,17 @@
+import { sortMethod } from '@/types/titleDesc.type';
+import styled from 'styled-components';
+
+export const TitleDescContainer = styled.div<{ $sort: sortMethod }>`
+  white-space: pre;
+  text-align: ${(props) => (props.$sort == 'left' ? 'left' : 'center')};
+
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    font-size: 1.25rem;
+    color: var(--desc-gray);
+  }
+`;
--- src/components/TitleDesc/TitleDesc.tsx
+++ src/components/TitleDesc/TitleDesc.tsx
@@ -1,7 +1,13 @@
 import { TitleDescProps } from '@/types/titleDesc.type';
+import { TitleDescContainer } from './TitleDesc.styled';
 
 function TitleDesc(props: TitleDescProps) {
-  return <h2>Hi</h2>;
+  return (
+    <TitleDescContainer $sort={props.sort}>
+      <h2>{props.title}</h2>
+      <p>{props.desc}</p>
+    </TitleDescContainer>
+  );
 }
 
 export default TitleDesc;
