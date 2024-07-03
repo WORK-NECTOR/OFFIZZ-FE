00ca2270d1e7c1c40896e2fd1b43dc9915e1d9fc
leehyewon0531 <ifpffk@naver.com>
2024-07-03 15:59:50 +0900

    OFZ-7 style: Button 스타일 작성

--- src/components/Button/Button.styled.ts
+++ src/components/Button/Button.styled.ts
@@ -1,6 +1,22 @@
+import { BtnSize, BtnType } from '@/types/button.type';
 import styled from 'styled-components';
 
-export const StyledBtn = styled.button`
-  height: 3.5rem;
-  padding: 0.125rem 0.25rem;
+export const StyledBtn = styled.button<{
+  $btnType: BtnType;
+  $btnSize: BtnSize;
+  $btnColor?: string;
+  $textColor: string;
+}>`
+  background-color: ${(props) =>
+    props.$btnType == 'empty' ? 'transparent' : props.$btnColor};
+  border: ${(props) =>
+    props.$btnType == 'full' ? 'none' : `1px solid ${props.$textColor}`};
+  color: ${(props) => props.$textColor};
+  border-radius: 3rem;
+  height: 2.75rem;
+  padding: 1rem 2rem;
+  font-size: 1.125rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,16 @@ import { ButtonProps } from '@/types/button.type';
 import { StyledBtn } from './Button.styled';
 
 function Button(props: ButtonProps) {
-  return <StyledBtn>{props.btnText}</StyledBtn>;
+  return (
+    <StyledBtn
+      $btnType={props.btnType}
+      $btnSize={props.btnSize}
+      $btnColor={props.btnColor}
+      $textColor={props.textColor}
+    >
+      {props.btnText}
+    </StyledBtn>
+  );
 }
 
 export default Button;
