961ee852a521e6185943b0bae66f0dc553c06afd
leehyewon0531 <ifpffk@naver.com>
2024-07-09 14:49:30 +0900

    OFZ-7 style: StatLabel 스타일 작성

--- src/components/StatLabel/StatLabel.styled.ts
+++ src/components/StatLabel/StatLabel.styled.ts
@@ -2,4 +2,8 @@ import styled from 'styled-components';
 
 export const StatLabelContainer = styled.div`
   height: 4.65rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: var(--desc-gray);
 `;
