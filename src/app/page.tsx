8a6d8aabcc1a9f9b5db67336f44728baa6d70042
leehyewon0531 <ifpffk@naver.com>
2024-07-20 16:38:15 +0900

    OFZ-7 feat: 메인 페이지 4번째 섹션 스타일 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Image from 'next/image';
 import Header from '@/components/Header';
 import thumbnail from '../../public/thumbnail.png';
 import TitleDesc from '@/components/TitleDesc';
-import { DASHBOARD_MAIN, TODO_MAIN, TOP_MAIN } from '@/constants/main';
+import {
+  DASHBOARD_MAIN,
+  RETROSPECT_MAIN,
+  TODO_MAIN,
+  TOP_MAIN,
+} from '@/constants/main';
 import Button from '@/components/Button';
 import { Fragment } from 'react';
 import styles from './page.module.css';
@@ -42,7 +47,9 @@ export default function MainPage() {
           </div>
         </section>
         <section className={styles['second-main']}>
-          <p className={styles['second-main-title']}>{DASHBOARD_MAIN.index}</p>
+          <p className={styles['main-index']} style={{ textAlign: 'center' }}>
+            {DASHBOARD_MAIN.index}
+          </p>
           <TitleDesc
             title={DASHBOARD_MAIN.title}
             desc={DASHBOARD_MAIN.desc}
@@ -59,7 +66,7 @@ export default function MainPage() {
         <section className={styles['third-main']}>
           <section className={styles['third-main-content']}>
             <section id="third-main-text">
-              <p className={styles['third-main-title']}>{TODO_MAIN.index}</p>
+              <p className={styles['main-index']}>{TODO_MAIN.index}</p>
               <TitleDesc
                 title={TODO_MAIN.title}
                 desc={TODO_MAIN.desc}
@@ -70,6 +77,26 @@ export default function MainPage() {
               src={thumbnail}
               alt="임시 이미지"
               className={styles['third-main-image']}
+            />
+          </section>
+        </section>
+        <section className={styles['fourth-main']}>
+          <Image
+            src={thumbnail}
+            alt="임시 이미지"
+            className={styles['fourth-main-image']}
+          />
+          <Image
+            src={thumbnail}
+            alt="임시 서브 이미지"
+            className={styles['fourth-main-sub-image']}
+          />
+          <section className={styles['fourth-main-text']}>
+            <p className={styles['main-index']}>{RETROSPECT_MAIN.index}</p>
+            <TitleDesc
+              title={RETROSPECT_MAIN.title}
+              desc={RETROSPECT_MAIN.desc}
+              sort="left"
             />
           </section>
         </section>
