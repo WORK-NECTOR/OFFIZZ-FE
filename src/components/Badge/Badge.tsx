1b08194b7f4b9ab7516f7152782afe333824ee2d
leehyewon0531 <ifpffk@naver.com>
2024-07-22 22:06:47 +0900

    OFZ-7 style: Badge 스타일 작업

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -7,7 +7,20 @@ function Badge(props: BadgeProps) {
     color = 'var(--blue-dark)',
   } = props;
 
-  return <div>{text}</div>;
+  return (
+    <div
+      style={{
+        width: 'max-content',
+        height: '1.625rem',
+        padding: '0.25rem 0.5rem',
+        borderRadius: '0.5rem',
+        backgroundColor,
+        color,
+      }}
+    >
+      {text}
+    </div>
+  );
 }
 
 export default Badge;
