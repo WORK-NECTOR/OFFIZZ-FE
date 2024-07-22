c6054cf29a3717adad0f966541c5a44ec0706cb4
leehyewon0531 <ifpffk@naver.com>
2024-07-22 22:14:57 +0900

    OFZ-7 style: PriceUnit 스타일 작업

--- src/components/PriceUnit/PriceUnit.tsx
+++ src/components/PriceUnit/PriceUnit.tsx
@@ -1,9 +1,15 @@
 import { PriceUnitProps } from '@/types/priceUnit.type';
+import { PriceUnitContainer } from './PriceUnit.styled';
 
 function PriceUnit(props: PriceUnitProps) {
   const { price, unit } = props;
 
-  return <div>Price</div>;
+  return (
+    <PriceUnitContainer>
+      <span id="price">{price.toLocaleString()}원&nbsp;</span>
+      <span id="unit">/&nbsp;{unit}</span>
+    </PriceUnitContainer>
+  );
 }
 
 export default PriceUnit;
