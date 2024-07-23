7eaaa17ce20e75ac2e209c6e48188c0e42d10d2f
yeonssu21 <kimys9295@naver.com>
2024-07-23 14:37:05 +0900

    OFZ-9 feat: 리캡 상세 제작

--- src/components/RecapBox/RecapBox.styled.ts
+++ src/components/RecapBox/RecapBox.styled.ts
@@ -4,5 +4,29 @@ export const RecapLayout = styled.div`
   width: 30.625rem;
   height: 17.7rem;
   border-radius: 16px;
-  background-color: #d9d9d9;
+  background-color: var(--blue-pale);
+  #recap-info-wrapper{
+    width: 15.875rem;
+    height: 6.565rem;
+    margin-top: 1.81rem;
+    margin-left: 1.5rem;
+  }
+  #recap-info-main{
+color: var(--black-main);
+font-family: Pretendard;
+font-size: 1.5rem;
+font-style: normal;
+font-weight: 700;
+line-height: 1.75rem;
+margin-bottom: 0.69rem;
+  }
+  #recap-info-sub{
+    color: var(--black-700);
+font-family: Pretendard;
+font-size: 1rem;
+font-style: normal;
+font-weight: 400;
+line-height: 1.375rem;
+
+  }
 `;
--- src/components/RecapBox/RecapBox.tsx
+++ src/components/RecapBox/RecapBox.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { RecapLayout } from './RecapBox.styled';
 import { RecapContentProps } from '@/types/recap.type';
+import Image from 'next/image';
 
 function RecapBox(props: RecapContentProps) {
   return (
-  <RecapLayout />
+  <RecapLayout>
+    <div id='recap-info-wrapper'>
+      <div id='recap-info-main'>{props.mainText}</div>
+      <div id='recap-info-sub'>{props.subText}</div>
+    </div>
+  </RecapLayout>
   );
 }
 
