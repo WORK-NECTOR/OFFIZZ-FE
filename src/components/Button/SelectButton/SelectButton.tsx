68a457ad5592a263391b73db604465c5ef794f43
leehyewon0531 <ifpffk@naver.com>
2024-07-27 16:03:48 +0900

    OFZ-7 feat: SelectButton 기본 세팅

---
+++ src/components/Button/SelectButton/SelectButton.tsx
@@ -0,0 +1,9 @@
+import { SelectButtonProps } from '@/types/selectButton.type';
+
+function SelectButton(props: SelectButtonProps) {
+  const { selected, btnHeight } = props;
+
+  return <button>HI</button>;
+}
+
+export default SelectButton;
---
+++ src/types/selectButton.type.ts
@@ -0,0 +1,4 @@
+export interface SelectButtonProps {
+  selected: boolean;
+  btnHeight: string;
+}
