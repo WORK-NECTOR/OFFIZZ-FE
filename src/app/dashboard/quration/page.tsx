d7b256ca079f0438f890670962533951d8156faf
yeonssu21 <kimys9295@naver.com>
2024-08-17 16:13:57 +0900

    OFZ-9 feat: 메인 박스 UI완료

--- src/app/dashboard/quration/page.tsx
+++ src/app/dashboard/quration/page.tsx
@@ -3,6 +3,8 @@ import Tab from '@/components/Tab/Tab'
 import React from 'react'
 import styled from 'styled-components';
 import styles from './page.module.css';
+import Image from 'next/image';
+import heart from '../../../../public/heart.png'
 const getWelcomeMessage = (activity: string, name: string, space: string) => {
     return (
       <div style={{marginLeft:'3.1rem'}}>
@@ -17,9 +19,17 @@ const getWelcomeMessage = (activity: string, name: string, space: string) => {
   const PlaceMainBox = () => {
     return (
       <PlaceMainContent>
+        <div style={{marginLeft:'2.81rem',display:'flex',marginTop:'17rem',justifyContent:'space-between'}}>
+        <div>
         <PMMainTitle>피츠케이크</PMMainTitle>
         <PMMainSub>강원도 강릉시 강릉대로 31</PMMainSub>
         <PMMainSub>강원도 강릉시 강릉대로 31</PMMainSub>
+        </div>
+        <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+            <Image src={heart} alt='좋아요' width={50} height={46}/>
+            <PMImagesub>가보고싶어요!</PMImagesub>
+        </div>
+        </div>
       </PlaceMainContent>
     );
   };
@@ -72,6 +82,7 @@ height: 25.5625rem;
 flex-shrink: 0;
 border-radius: 1rem;
 background: #E5E5E5;
+padding-right: 2.44rem;
 `;
 const PMMainTitle = styled.div`
 color: var(--Greyscale-Black, #000);
@@ -87,5 +98,16 @@ font-family: Inter;
 font-size: 1rem;
 font-style: normal;
 font-weight: 400;
+line-height: normal;
+margin-top: 0.87rem;
+`;
+const PMImagesub = styled.div`
+margin-top: 1rem;
+color: var(--Greyscale-Black, #000);
+text-align: center;
+font-family: SUIT;
+font-size: 0.875rem;
+font-style: normal;
+font-weight: 700;
 line-height: normal;
 `;
