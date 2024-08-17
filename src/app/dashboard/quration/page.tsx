fed9cc925a09b5062bb9921635c2d285d797d00a
yeonssu21 <kimys9295@naver.com>
2024-08-17 15:58:12 +0900

    OFZ-9 feat: 대시보드 메인 박스 UI

--- src/app/dashboard/quration/page.tsx
+++ src/app/dashboard/quration/page.tsx
@@ -14,6 +14,15 @@ const getWelcomeMessage = (activity: string, name: string, space: string) => {
       </div>
     );
   };
+  const PlaceMainBox = () => {
+    return (
+      <PlaceMainContent>
+        <PMMainTitle>피츠케이크</PMMainTitle>
+        <PMMainSub>강원도 강릉시 강릉대로 31</PMMainSub>
+        <PMMainSub>강원도 강릉시 강릉대로 31</PMMainSub>
+      </PlaceMainContent>
+    );
+  };
 function page() {
 const activity = "영화";
   const name = "홍길동";
@@ -28,6 +37,10 @@ const activity = "영화";
         <div className={styles['now-title']}>현재위치</div>
                     <div className={styles['now-desc']}>{nowLocation}</div>
                     <div className={styles['now-suggest']}>현재 위치가 아니신가요?</div>
+        </div>
+        <div style={{display:'flex',justifyContent:'space-between',margin:'1.5rem 3rem 0'}}>
+        <PlaceMainBox />
+        <PlaceMainBox />
         </div>
       </div>
     </div>
@@ -51,5 +64,28 @@ font-family: Inter;
 font-size: 1.5rem;
 font-style: normal;
 font-weight: 500;
+line-height: normal;
+`;
+const PlaceMainContent = styled.div`
+width: 31rem;
+height: 25.5625rem;
+flex-shrink: 0;
+border-radius: 1rem;
+background: #E5E5E5;
+`;
+const PMMainTitle = styled.div`
+color: var(--Greyscale-Black, #000);
+font-family: Inter;
+font-size: 1.5rem;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`;
+const PMMainSub = styled.div`
+color: var(--Greyscale-Black, #000);
+font-family: Inter;
+font-size: 1rem;
+font-style: normal;
+font-weight: 400;
 line-height: normal;
 `;
