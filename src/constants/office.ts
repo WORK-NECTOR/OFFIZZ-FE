e6c624b260d1affb446b397950fcde65633bffe4
leehyewon0531 <ifpffk@naver.com>
2024-08-17 19:06:43 +0900

    OFZ-7 chore: 지역 배열 이름 변경

--- src/constants/office.ts
+++ src/constants/office.ts
@@ -1,13 +1,9 @@
 export const regionArr = [
-  { id: 1, region: '서울' },
-  { id: 2, region: '부산' },
-  { id: 3, region: '인천' },
-  { id: 4, region: '경기' },
-  { id: 5, region: '대구' },
-  { id: 6, region: '대전' },
-  { id: 7, region: '충청' },
-  { id: 8, region: '경상' },
-  { id: 9, region: '기타' },
+  { id: 1, region: '수도권' },
+  { id: 2, region: '경상권' },
+  { id: 3, region: '제주권' },
+  { id: 4, region: '충청권' },
+  { id: 5, region: '전라권' },
 ];
 
 export const OFFICE_INFO_TITLE = {
