cad215f1b1f9a68e59af9709161e22dea9878b3d
leehyewon0531 <ifpffk@naver.com>
2024-08-18 23:05:51 +0900

    OFZ-23 chore: query 테스트 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '@/services/queryClient';
 
 export const metadata: Metadata = {
   title: 'OFFIZZ',
@@ -12,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body>{children}</body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="ko">
+        <body>{children}</body>
+      </html>
+    </QueryClientProvider>
   );
 }
