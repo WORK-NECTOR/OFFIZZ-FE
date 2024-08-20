46ed2f41e972d7fc607f44826600a429f231b643
leehyewon0531 <ifpffk@naver.com>
2024-08-20 17:00:59 +0900

    OFZ-25 chore: 추천 페이지 상수 수정

--- src/constants/recommend.ts
+++ src/constants/recommend.ts
@@ -1,5 +1,6 @@
-export const RECOMMEND_SEARCH = {
-  title: '어디로 떠나볼까요?',
-  desc: '지역명, 주소, 숙소명으로 공유오피스를 검색해 보세요',
-  placeholder: '제주도 서귀포시',
+export const RECOMMEND_TEXT = {
+  searchTitle: '어디로 떠나볼까요?',
+  searchDesc: '지역명, 주소, 숙소명으로 공유오피스를 검색해 보세요',
+  searchPlaceholder: '제주도 서귀포시',
+  contentTitle: '추천 지역',
 };
