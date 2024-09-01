35c1ca10f3f60bed6605628d3206a00f9faecd79
leehyewon0531 <ifpffk@naver.com>
2024-09-01 18:08:54 +0900

    OFZ-26 chore: getOffice 엔드포인트 재수정

--- src/api/office/getOffice.ts
+++ src/api/office/getOffice.ts
@@ -28,5 +28,5 @@ export interface GetAllOfficeResponse extends GetOfficeResponse {
 export const getRecRegionOffice = (params: GetRecRegionOfficeParams) => {
   const { region = '수도권', size = 4 } = params;
 
-  return instance.get<GetOfficeResponse>(`work/office/rec/${region}/${size}`);
+  return instance.get<GetOfficeResponse>(`/work/office/rec/${region}/${size}`);
 };
