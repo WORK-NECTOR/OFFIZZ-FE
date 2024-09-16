0cefb61975c68e621b6bb3695167663e330f0a17
leehyewon0531 <ifpffk@naver.com>
2024-09-16 17:55:45 +0900

    OFZ-34 style: 온보딩 페이지 기본 틀 생성

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+function OnboardingPage() {
+  return (
+    <div className={styles['background']}>
+      <div className={styles['container']}></div>
+    </div>
+  );
+}
+
+export default OnboardingPage;
