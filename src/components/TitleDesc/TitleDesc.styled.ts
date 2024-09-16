8a77a0cb2d680dde951c3171daaa908445615b5d
leehyewon0531 <ifpffk@naver.com>
2024-09-16 20:34:04 +0900

    OFZ-34 fix: TitleDesc 컴포넌트 수정

--- src/components/TitleDesc/TitleDesc.styled.ts
+++ src/components/TitleDesc/TitleDesc.styled.ts
@@ -3,19 +3,20 @@ import { SortMethod } from '@/types/titleDesc.type';
 
 export const TitleDescContainer = styled.div<{
   $sort: SortMethod;
-  $themeName: string;
+  $titleSize: string;
+  $descSize: string;
 }>`
   white-space: pre;
   text-align: ${(props) => (props.$sort === 'left' ? 'left' : 'center')};
 
   h2 {
-    font-size: 2rem;
+    font-size: ${(props) => props.$titleSize || '2rem'};
     font-size: ${(props) => props.theme.recap || '2rem'};
     margin-bottom: 1.25rem;
   }
 
   p {
-    font-size: 1.25rem;
+    font-size: ${(props) => props.$descSize || '1.25rem'};
     color: var(--desc-gray);
     font-weight: ${(props) => props.theme.recap};
     font-family: ${(props) => props.theme.recap};
--- src/components/TitleDesc/TitleDesc.tsx
+++ src/components/TitleDesc/TitleDesc.tsx
@@ -3,10 +3,14 @@ import { TitleDescProps } from '@/types/titleDesc.type';
 import { TitleDescContainer } from './TitleDesc.styled';
 
 function TitleDesc(props: TitleDescProps) {
-  const { sort, title, desc } = props;
+  const { sort, title, desc, titleSize = '2rem', descSize = '1.25rem' } = props;
 
   return (
-    <TitleDescContainer $sort={sort}>
+    <TitleDescContainer
+      $sort={sort}
+      $titleSize={titleSize}
+      $descSize={descSize}
+    >
       <h2>{title}</h2>
       <p>{desc}</p>
     </TitleDescContainer>
