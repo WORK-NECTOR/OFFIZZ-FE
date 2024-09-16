efc80889b26debec22728a9b887a17a18b70cb3c
yeonssu21 <kimys9295@naver.com>
2024-09-16 18:17:48 +0900

    OFZ-32 chore: format:fix

--- src/app/Information/page.tsx
+++ src/app/Information/page.tsx
@@ -3,17 +3,20 @@
 import React from 'react';
 import Tab from '@/components/Tab/Tab';
 import styles from './page.module.css';
-import MainCharacterBox from './components/MainCharacterBox'
+import MainCharacterBox from './components/MainCharacterBox';
+
 function InformationPage() {
   return (
-    <div style={{display:'flex'}}>
+    <div style={{ display: 'flex' }}>
       <Tab />
-      <div style={{border:'1px solid red',width:'100%'}}>
+      <div style={{ border: '1px solid red', width: '100%' }}>
         <div className={styles.Top}>
-        <div className={styles.Day}>day1</div>
-        <div className={styles.Switch}>SwitchBtn</div>
+          <div className={styles.Day}>day1</div>
+          <div className={styles.Switch}>SwitchBtn</div>
         </div>
-        <div><MainCharacterBox/></div>
+        <div>
+          <MainCharacterBox />
+        </div>
       </div>
     </div>
   );
--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { TabContainer, TabListContainer } from './Tab.styled';
 import Link from 'next/link';
+import { TabContainer, TabListContainer } from './Tab.styled';
 
 function Tab() {
   return (
     <TabContainer>
       <div id="tab-title">OFIZZ</div>
       <TabListContainer>
-      <Link href="/information" passHref>
-        <div id="tab-list">홈</div>
+        <Link href="/information" passHref>
+          <div id="tab-list">홈</div>
         </Link>
         <Link href="/information/quration" passHref>
-        <div id="tab-list">큐레이션</div>
+          <div id="tab-list">큐레이션</div>
         </Link>
         <div id="tab-list">회고</div>
         <div id="tab-list">마이</div>
