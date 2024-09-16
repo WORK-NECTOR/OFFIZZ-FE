7bd5a32826cf09fbd1f51955e465ce717bbfee19
leehyewon0531 <ifpffk@naver.com>
2024-09-16 20:39:06 +0900

    OFZ-34 feat: 온보딩 0번째 페이지 완성

--- src/app/onboarding/components/OnboardingStart/Onboarding.styled.ts
+++ src/app/onboarding/components/OnboardingStart/Onboarding.styled.ts
@@ -3,3 +3,12 @@ import styled from 'styled-components';
 export const StartContainer = styled.div`
   display: flex;
 `;
+
+export const StartTextWrapper = styled.div`
+  width: 25.5625rem;
+  height: 26.5625rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2.25rem;
+`;
