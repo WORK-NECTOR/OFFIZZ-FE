f83383948124e9196dd28468093d26eac3b246c0
leehyewon0531 <ifpffk@naver.com>
2024-09-16 20:02:41 +0900

    OFZ-34 feat: 온보딩 0번째 페이지 연결

---
+++ src/app/onboarding/components/OnboardingStart/OnboardingStart.tsx
@@ -0,0 +1,7 @@
+import { StartContainer } from './Onboarding.styled';
+
+function OnboardingStart() {
+  return <StartContainer>Hi</StartContainer>;
+}
+
+export default OnboardingStart;
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,12 +1,18 @@
+'use client';
 import useStepstore from '@/store/useStepStore';
 import styles from './page.module.css';
+import OnboardingStart from './components/OnboardingStart';
+
+const stepPage: { [key: number]: JSX.Element } = {
+  0: <OnboardingStart />,
+};
 
 function OnboardingPage() {
   const { step } = useStepstore();
 
   return (
     <div className={styles['background']}>
-      <div className={styles['container']}></div>
+      <div className={styles['container']}>{stepPage[step]}</div>
     </div>
   );
 }
