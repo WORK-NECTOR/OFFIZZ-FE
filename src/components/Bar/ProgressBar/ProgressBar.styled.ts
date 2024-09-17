bfbf0f546dc44fe13be4ab2c600566583702b5aa
leehyewon0531 <ifpffk@naver.com>
2024-09-17 17:16:50 +0900

    OFZ-34 style: ProgressBar 스타일 수정

--- src/components/Bar/ProgressBar/ProgressBar.styled.ts
+++ src/components/Bar/ProgressBar/ProgressBar.styled.ts
@@ -4,6 +4,24 @@ export const ProgressBarContainer = styled.div``;
 
 export const StyledProgressBar = styled.progress<{ $width: string }>`
   width: ${(props) => props.$width || '41.25rem'};
+  height: 0.5rem;
+  -webkit-appearance: none;
+  appearance: none;
+
+  &::-webkit-progress-bar {
+    background: var(--Greyscale-50, #f5f5f5);
+    border-radius: 1.5rem;
+  }
+
+  &::-webkit-progress-value {
+    background: linear-gradient(270deg, #f0b1e1 0%, #498df2 100%);
+    border-radius: 1.5rem;
+  }
+
+  &::-moz-progress-bar {
+    background: linear-gradient(270deg, #f0b1e1 0%, #498df2 100%);
+    border-radius: 1.5rem;
+  }
 `;
 
 export const ProgressStep = styled.div`
