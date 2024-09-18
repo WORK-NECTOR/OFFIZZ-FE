204033d6baa3770a6612d32e8da662b95c2ef3f6
leehyewon0531 <ifpffk@naver.com>
2024-09-18 17:15:07 +0900

    OFZ-34 feat: 온보딩 3번째 페이지 BtnContainer 추가

--- src/app/onboarding/components/OnboardingPlace/OnboardingPlace.styled.ts
+++ src/app/onboarding/components/OnboardingPlace/OnboardingPlace.styled.ts
@@ -8,3 +8,10 @@ export const PlaceContainer = styled.div`
     margin: 2.5rem 0 1.75rem 0;
   }
 `;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  position: absolute;
+  bottom: 0;
+`;
--- src/app/onboarding/components/OnboardingPlace/OnboardingPlace.tsx
+++ src/app/onboarding/components/OnboardingPlace/OnboardingPlace.tsx
@@ -1,10 +1,17 @@
 import ProgressBar from '@/components/Bar/ProgressBar';
-import { PlaceContainer } from './OnboardingPlace.styled';
+import { PlaceContainer, BtnContainer } from './OnboardingPlace.styled';
 import TitleDesc from '@/components/TitleDesc';
 import { ONBOARDING_DESC } from '@/constants/onboarding';
 import SearchBar from '@/components/Bar/SearchBar';
+import BackButton from '@/components/Button/BackButton';
+import useStepstore from '@/store/useStepStore';
+import BasicButton from '@/components/Button/BasicButton';
+import useOnboardingStore from '@/store/useOnboardingStore';
 
 function OnboardingPlace() {
+  const { incrementStep, decrementStep } = useStepstore();
+  const { place } = useOnboardingStore();
+
   return (
     <PlaceContainer>
       <ProgressBar width="41.25rem" current={3} total={8} />
@@ -20,6 +27,19 @@ function OnboardingPlace() {
         height="2.625rem"
         placeholder={ONBOARDING_DESC.placePlaceholder}
       />
+      <BtnContainer>
+        <BackButton clickHandler={decrementStep} />
+        <BasicButton
+          btnText={ONBOARDING_DESC.nextBtn}
+          btnType="full"
+          btnColor={place ? 'var(--blue-main)' : 'var(--black-50)'}
+          textColor={place ? 'var(--white-main)' : 'var(--black-300)'}
+          btnHeight="2.5rem"
+          padding="0.5rem 1rem"
+          fontSize="1rem"
+          clickHandler={place ? incrementStep : () => {}}
+        />
+      </BtnContainer>
     </PlaceContainer>
   );
 }
