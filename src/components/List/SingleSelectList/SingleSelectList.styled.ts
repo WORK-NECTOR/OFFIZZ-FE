b649a41fdfffde4a4891ca46380be3021ae4b272
leehyewon0531 <ifpffk@naver.com>
2024-09-22 21:31:16 +0900

    OFZ-34 style: SingleSelectList 스타일 작업

--- src/components/List/SingleSelectList/SingleSelectList.styled.ts
+++ src/components/List/SingleSelectList/SingleSelectList.styled.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
 
-export const ListContainer = styled.ul``;
+export const ListContainer = styled.ul`
+  width: 20rem;
+  border: 1px solid var(--black-100);
+  border-radius: 0.5rem;
+
+  li {
+    padding: 0.75rem 1rem;
+    border-bottom: 1px solid var(--black-100);
+  }
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
--- src/components/List/SingleSelectList/SingleSelectList.tsx
+++ src/components/List/SingleSelectList/SingleSelectList.tsx
@@ -1,10 +1,20 @@
 import { SingleSelectListProps } from '@/types/list.type';
 import { ListContainer } from './SingleSelectList.styled';
+import IconTitle from '@/components/IconTitle';
 
 function SingleSelectList(props: SingleSelectListProps) {
   const { listArr, selectFunc } = props;
 
-  return <ListContainer></ListContainer>;
+  return (
+    <ListContainer>
+      {listArr.length > 0 &&
+        listArr.map((el, idx) => (
+          <li key={idx}>
+            <IconTitle icon={el.icon} title={el.title} />
+          </li>
+        ))}
+    </ListContainer>
+  );
 }
 
 export default SingleSelectList;
