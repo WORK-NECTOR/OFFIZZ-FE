7b3a56d8c0fe132cdb3a623b750bcb10273aa3c0
leehyewon0531 <ifpffk@naver.com>
2024-09-22 21:37:52 +0900

    OFZ-34 feat: SingleSelectList 클릭 로직 추가

--- src/components/List/SingleSelectList/SingleSelectList.styled.ts
+++ src/components/List/SingleSelectList/SingleSelectList.styled.ts
@@ -5,12 +5,16 @@ export const ListContainer = styled.ul`
   border: 1px solid var(--black-100);
   border-radius: 0.5rem;
 
-  li {
-    padding: 0.75rem 1rem;
-    border-bottom: 1px solid var(--black-100);
-  }
-
   &:last-child {
     border-bottom: none;
   }
+`;
+
+export const ListEl = styled.li<{ $selected: boolean }>`
+  padding: 0.75rem 1rem;
+  border-bottom: 1px solid var(--black-100);
+  border: ${(props) => (props.$selected ? '1px solid var(--blue-main)' : '')};
+  background-color: ${(props) =>
+    props.$selected ? 'var(--Blue-pale, #EEF7FF)' : ''};
+  cursor: pointer;
 `;
--- src/components/List/SingleSelectList/SingleSelectList.tsx
+++ src/components/List/SingleSelectList/SingleSelectList.tsx
@@ -1,17 +1,28 @@
 import { SingleSelectListProps } from '@/types/list.type';
-import { ListContainer } from './SingleSelectList.styled';
+import { ListContainer, ListEl } from './SingleSelectList.styled';
 import IconTitle from '@/components/IconTitle';
+import { useState } from 'react';
 
 function SingleSelectList(props: SingleSelectListProps) {
   const { listArr, selectFunc } = props;
+  const [selected, setSelected] = useState('');
+
+  function handleClick(title: string) {
+    setSelected(title);
+    selectFunc(title);
+  }
 
   return (
     <ListContainer>
       {listArr.length > 0 &&
         listArr.map((el, idx) => (
-          <li key={idx}>
+          <ListEl
+            key={idx}
+            onClick={() => handleClick(el.title)}
+            $selected={selected === el.title}
+          >
             <IconTitle icon={el.icon} title={el.title} />
-          </li>
+          </ListEl>
         ))}
     </ListContainer>
   );
