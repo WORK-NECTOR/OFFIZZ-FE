5febc83787621d44e1299f5d9ffa58c6640e6b43
yeonssu21 <kimys9295@naver.com>
2024-09-23 19:51:02 +0900

    OFZ-37 chore: config 설정

---
+++ src/app/information/components/Recommend/Recommend.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+import { RecommendBox } from './Recommend.styled'
+
+function Recommend() {
+  return (
+    <RecommendBox>
+      오늘은 여기서 일해보는거 어때요?
+      <Link href="/information/quration">더 찾아보기</Link>
+    </RecommendBox>
+  );
+}
+
+export default Recommend;
