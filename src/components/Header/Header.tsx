aae274442a2e9de7eafe8285c25e65cbd18247c4
leehyewon0531 <ifpffk@naver.com>
2024-09-28 17:28:22 +0900

    OFZ-45 fix: 헤더 메뉴 한글 수정 및 라우트 로직 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,11 +10,20 @@ import { HeaderTextUnion } from '@/types/header.type';
 
 function Header() {
   const router = useRouter();
-  const { selectedMenu, setSelectedMenu } = useHeaderMenuStore.getState();
+  const { selectedMenu, setSelectedMenu } = useHeaderMenuStore();
+
+  const menuToRouteMap: Record<HeaderTextUnion, string> = {
+    [HEADER_TEXT.home]: '/',
+    [HEADER_TEXT.recommendation]: '/recommend',
+    [HEADER_TEXT.recap]: '/recap',
+  };
 
   const handleClick = (e: React.MouseEvent<HTMLLIElement>) => {
     const text = e.currentTarget.innerText as HeaderTextUnion;
-    setSelectedMenu(text);
+    setSelectedMenu(text as HeaderTextUnion);
+
+    const route = menuToRouteMap[text];
+    if (route) router.push(route);
   };
 
   return (
