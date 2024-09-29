5273f3065fea580b948d63b05bf0d0b1577e58d4
leehyewon0531 <ifpffk@naver.com>
2024-09-29 21:03:41 +0900

    OFZ-46 style: FeatureInfo 스타일 작업

--- src/components/FeatureInfo/FeatureInfo.styled.ts
+++ src/components/FeatureInfo/FeatureInfo.styled.ts
@@ -1,3 +1,37 @@
 import styled from 'styled-components';
 
-export const FeatureInfoContainer = styled.div``;
+export const FeatureInfoContainer = styled.div`
+  width: max-content;
+  display: flex;
+  align-items: center;
+  gap: 1.625rem;
+`;
+
+export const DottedLine = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    width: 0.375rem;
+    height: 0.375rem;
+    border-radius: 50%;
+    background-color: var(--black-main);
+  }
+`;
+
+export const EnTitleDesc = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+
+  h3 {
+    font-family: Figtree;
+    font-size: 1.25rem;
+    font-weight: 600;
+  }
+
+  p {
+    font-size: 1rem;
+    color: var(--black-400);
+  }
+`;
--- src/components/FeatureInfo/FeatureInfo.tsx
+++ src/components/FeatureInfo/FeatureInfo.tsx
@@ -1,10 +1,27 @@
 import { FeatureInfoProps } from '@/types/feature.type';
-import { FeatureInfoContainer } from './FeatureInfo.styled';
+import {
+  DottedLine,
+  EnTitleDesc,
+  FeatureInfoContainer,
+} from './FeatureInfo.styled';
+import Image from 'next/image';
+import line from 'public/dotted-line.svg';
 
 function FeatureInfo(props: FeatureInfoProps) {
   const { title, desc } = props;
 
-  return <FeatureInfoContainer></FeatureInfoContainer>;
+  return (
+    <FeatureInfoContainer>
+      <DottedLine>
+        <Image src={line} alt="점선" />
+        <div></div>
+      </DottedLine>
+      <EnTitleDesc>
+        <h3>{title}</h3>
+        <p>{desc}</p>
+      </EnTitleDesc>
+    </FeatureInfoContainer>
+  );
 }
 
 export default FeatureInfo;
