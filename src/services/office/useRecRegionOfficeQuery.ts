068e6dcd1ff0178d168b048fac6b0c7cc81c28ec
leehyewon0531 <ifpffk@naver.com>
2024-10-01 16:26:51 +0900

    OFZ-47 feat: 추천 페이지 추천 지역 쿼리 작성

--- src/services/office/useRecRegionOfficeQuery.ts
+++ src/services/office/useRecRegionOfficeQuery.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import {
+  GetAllRecRegionOfficeParams,
   GetRecRegionOfficeParams,
+  getRecAllRegionOffice,
   getRecRegionOffice,
 } from '@/api/office/getOffice';
 
@@ -16,3 +18,19 @@ export const useRecRegionOfficeQuery = (params: GetRecRegionOfficeParams) =>
     },
     staleTime: 1000 * 20,
   });
+
+export const useAllRecRegionOfficeQuery = (
+  params: GetAllRecRegionOfficeParams,
+) => {
+  useQuery({
+    queryKey: ['office', params],
+    queryFn: async () => {
+      const response = await getRecAllRegionOffice(params);
+      const { data } = response;
+      const { recOffices } = data;
+
+      return recOffices;
+    },
+    staleTime: 1000 * 20,
+  });
+};
