562c72c17d9f97f9cc361cba2dfdf5dd5c64703c
leehyewon0531 <ifpffk@naver.com>
2024-10-01 16:52:41 +0900

    OFZ-47 feat: 추천 페이지 기본 틀 작업

--- src/app/recommend/page.tsx
+++ src/app/recommend/page.tsx
@@ -7,8 +7,29 @@ import TitleDesc from '@/components/TitleDesc';
 import { RECOMMEND_TEXT } from '@/constants/recommend';
 import Image from 'next/image';
 import ic_search from 'public/search.png';
+import { useState } from 'react';
+import { regionArr } from '@/constants/office';
+import SelectButton from '@/components/Button/SelectButton';
+import useRegionStore, { Region } from '@/store/useRegionStore';
+import { useAllRecRegionOfficeQuery } from '@/services/office/useRecRegionOfficeQuery';
+import OfficeAccordion from '@/components/OfficeAccordion';
 
 function RecommendPage() {
+  const [searchInput, setSearchInput] = useState('');
+  const [recPage, setRecPage] = useState(1);
+  const [searchPage, setSearchPage] = useState(1);
+  const { selectedRegion, setSelectedRegion } = useRegionStore();
+  const { data: recData } = useAllRecRegionOfficeQuery({
+    region: selectedRegion,
+    page: recPage,
+    size: 8,
+  });
+
+  const clickHandler = (e: React.MouseEvent<HTMLElement>) => {
+    const text = e.currentTarget.innerText as Region;
+    setSelectedRegion(text);
+  };
+
   return (
     <main className={styles['recommend-main']}>
       <Header />
@@ -24,8 +45,47 @@ function RecommendPage() {
             alt="검색 아이콘"
             className={styles['ic-search']}
           />
-          <input type="text" className={styles['search-input']} />
+          <input
+            type="text"
+            className={styles['search-input']}
+            placeholder={RECOMMEND_TEXT.searchPlaceholder}
+          />
         </div>
+      </section>
+      <section className={styles['result-section']}>
+        {!searchInput && (
+          <div className={styles['recommend-result-container']}>
+            <h2>{RECOMMEND_TEXT.contentTitle}</h2>
+            <ul className={styles['recommend-btn-list']}>
+              {regionArr.map((item) => (
+                <li key={item.id}>
+                  <SelectButton
+                    selected={selectedRegion === item.region}
+                    btnText={item.region}
+                    btnHeight="2.5rem"
+                    clickHandler={clickHandler}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {recData && recData.recOffices.length > 0 && (
+          <div className={styles['recommend-result-container']}>
+            {recData.recOffices.map((el) => (
+              <OfficeAccordion
+                placeName={el.name}
+                placeAddress={el.address}
+                allYearRound={el.facilities.openAllYear}
+                dayAndNight={el.facilities.twentyFourHoursOperation}
+                price={el.price}
+                priceUnit={el.priceType}
+                key={el.officeId}
+                officeId={el.officeId}
+              />
+            ))}
+          </div>
+        )}
       </section>
       <Footer />
     </main>
