8711658768383485de5974ee85c290c35be59653
yeonssu21 <kimys9295@naver.com>
2024-10-05 17:49:33 +0900

    OFZ-51 feat: 작성 유무 ui

--- src/app/information/recap/page.tsx
+++ src/app/information/recap/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 import Tab from '@/components/Tab/Tab';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './page.module.css';
 import leftarrow from '../../../../public/leftarrow.png';
 import rightarrow from '../../../../public/rightarrow.png';
 
 function RecapPage() {
+  const [write,setWrite] = useState(false)
   const conditions = [
     '최고였어',
     '좋았어',
@@ -14,6 +15,9 @@ function RecapPage() {
     '별로였어',
     '힘들었어',
   ];
+  const onClickWrite =()=>{
+    setWrite(true)
+  }
 
   return (
     <div style={{ display: 'flex' }}>
@@ -36,7 +40,7 @@ function RecapPage() {
           <div className={styles.recapWrapper}>
             <div className={styles.recapTop}>
               <div className={styles.recapTitle}>회고</div>
-              <div className={styles.recapBtn}>작성</div>
+              <div className={styles.recapBtn} onClick={onClickWrite}>작성</div>
             </div>
             <div className={styles.recapMiddle}>
               <div style={{display:'flex' ,justifyContent:'space-between'}}>
@@ -49,13 +53,15 @@ function RecapPage() {
                   </div>
                   <div className={styles.middleSubBox}>
                     <div className={styles.middleSubTime}>8시간</div>
-                    <div style={{display:'flex'}}>
+                    {!write && <div className={styles.notWrite}>아직 작성 전이에요.</div>}
+                    {write && <div style={{display:'flex'}}>
                     {conditions.map((condition, index) => (
                       <div key={index} className={styles.middleSubCondition}>
                         {condition}
                       </div>
                     ))}
-                    </div>
+                    </div>}
+                    
                   </div>
                   <div></div>
                 </div>
@@ -69,37 +75,45 @@ function RecapPage() {
                   </div>
                   <div className={styles.middleSubBox}>
                     <div className={styles.middleSubTime}>8시간</div>
-                    <div style={{display:'flex'}}>
+                    {!write && <div className={styles.notWrite}>아직 작성 전이에요.</div>}
+                    {write && <div style={{display:'flex'}}>
                     {conditions.map((condition, index) => (
                       <div key={index} className={styles.middleSubCondition}>
                         {condition}
                       </div>
                     ))}
-                    </div>
+                    </div>}
                   </div>
                 </div>
               </div>
               </div>
             </div>
             {/* 미들 */}
-            <div style={{marginTop:'3rem'}}>
+            {write ? <>
+              <div style={{marginTop:'3rem'}}>
             <div className={styles.middleTitle}>여행 기록</div>
             <div className={styles.photo}>사진</div>
             </div>
+            </>:''}
             {/* 마지막 */}
             <div className={styles.bottom}>
               <div className={styles.bottomTitle}>YWT 회고</div>
               <div className={styles.bottominputBox}>
                 <div className={styles.bottomSub}>Y 한 일</div>
-                <input className={styles.bottomInput} placeholder='오늘 한 일을 작성해주세요.'></input>
+                {!write && <div className={styles.bottomNotWrite}>아직 작성 전이에요.</div>}
+                {write && <input className={styles.bottomInput} placeholder='오늘 한 일을 작성해주세요.'></input>}
+                
               </div>
               <div className={styles.bottominputBox}>
               <div className={styles.bottomSub}>W 배운 점</div>
-              <input className={styles.bottomInput} placeholder='오늘 배운 점을 작성해주세요.'></input>
+              {!write && <div className={styles.bottomNotWrite}>아직 작성 전이에요.</div>}
+              {write &&               <input className={styles.bottomInput} placeholder='오늘 배운 점을 작성해주세요.'></input>}
               </div>
               <div className={styles.bottominputBox}>
               <div className={styles.bottomSub}>T 다음에 할 일</div>
-              <input className={styles.bottomInput} placeholder='다음에 할 일을 작성해주세요.'></input>
+              {!write && <div className={styles.bottomNotWrite}>아직 작성 전이에요.</div>}
+              {write && <input className={styles.bottomInput} placeholder='다음에 할 일을 작성해주세요.'></input>}
+              
               </div>
             </div>
           </div>
