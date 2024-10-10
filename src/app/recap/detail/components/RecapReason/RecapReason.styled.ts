8a6ddddf2ed8845412b171c2e26251445eebcf57
leehyewon0531 <ifpffk@naver.com>
2024-10-10 21:26:20 +0900

    OFZ-53 chore: 브랜치 변경을 위한 중간 저장

--- src/app/recap/detail/components/RecapReason/RecapReason.styled.ts
+++ src/app/recap/detail/components/RecapReason/RecapReason.styled.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const RecapReasonContainer = styled.div``;
+export const RecapReasonContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 2rem 1.75rem;
+`;
--- src/app/recap/detail/components/RecapReason/RecapReason.tsx
+++ src/app/recap/detail/components/RecapReason/RecapReason.tsx
@@ -1,9 +1,12 @@
+import { RECAP_DETAIL_TITLE } from '@/constants/recap';
 import BackNextBtn from '../BackNextBtn';
+import TitleLine from '../TitleLine';
 import { RecapReasonContainer } from './RecapReason.styled';
 
 function RecapReason() {
   return (
     <RecapReasonContainer>
+      <TitleLine title={RECAP_DETAIL_TITLE.reason} />
       <BackNextBtn />
     </RecapReasonContainer>
   );
