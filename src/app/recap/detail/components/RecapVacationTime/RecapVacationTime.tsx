8bd21d1d222e99521a898e5594e5e4d602758822
leehyewon0531 <ifpffk@naver.com>
2024-10-12 18:04:06 +0900

    OFZ-53 chore: 브랜치 이동을 위한 중간 저장

--- src/app/recap/detail/components/RecapVacationTime/RecapVacationTime.tsx
+++ src/app/recap/detail/components/RecapVacationTime/RecapVacationTime.tsx
@@ -1,11 +1,23 @@
 import { RECAP_DETAIL_TITLE } from '@/constants/recap';
 import TitleLine from '../TitleLine';
 import { VacationTimeContainer } from './RecapVacationTime.styled';
+import TextContainer from '../TextContainer';
+import RecapText from '../RecapText';
+import useRecapStore from '@/store/useRecapStore';
 
 function RecapVacationTime() {
+  const { recapDetailData } = useRecapStore();
+  const data = recapDetailData.fifthPage;
+
   return (
     <VacationTimeContainer>
       <TitleLine title={RECAP_DETAIL_TITLE.vacationTime} />
+      <TextContainer>
+        <RecapText text="워케이션 기간동안" />
+        <div style={{ display: 'flex' }}>
+          <RecapText text={`${data.bestWorkDays}`} />
+        </div>
+      </TextContainer>
     </VacationTimeContainer>
   );
 }
