ca510b324873fd2190c63967bcccaec830c4f8f3
leehyewon0531 <ifpffk@naver.com>
2024-10-13 14:29:49 +0900

    OFZ-53 feat: RecapEnd 페이지 완성

--- src/app/recap/detail/components/RecapEnd/RecapEnd.styled.ts
+++ src/app/recap/detail/components/RecapEnd/RecapEnd.styled.ts
@@ -3,4 +3,46 @@ import styled from 'styled-components';
 export const RecapEndContainer = styled.div`
   width: 100%;
   height: 100%;
+  position: relative;
+
+  #recap-img {
+    width: 12.5rem;
+    height: 12.5rem;
+    opacity: 0;
+    animation: fade 1s forwards;
+    position: absolute;
+    top: 5rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  #recap-end-text {
+    opacity: 0;
+    animation: fade 2.5s forwards;
+    position: absolute;
+    bottom: 4rem;
+    left: 50%;
+    transform: translateX(-50%);
+    white-space: pre;
+    text-align: center;
+  }
+
+  #recap-guide-msg {
+    font-size: 0.75rem;
+    color: var(--black-200);
+    opacity: 0;
+    animation: fade 2.5s forwards;
+    position: absolute;
+    bottom: 2.8rem;
+    left: 50%;
+    transform: translateX(-50%);
+    white-space: pre;
+    text-align: center;
+  }
+
+  @keyframes fade {
+    to {
+      opacity: 1;
+    }
+  }
 `;
--- src/app/recap/detail/components/RecapEnd/RecapEnd.tsx
+++ src/app/recap/detail/components/RecapEnd/RecapEnd.tsx
@@ -1,10 +1,29 @@
+import { useEffect } from 'react';
 import TitleLine from '../TitleLine';
 import { RecapEndContainer } from './RecapEnd.styled';
+import Image from 'next/image';
+import ic_recap from 'public/workation-fizz-retrospect.png';
+import { useRouter } from 'next/navigation';
+import useStepstore from '@/store/useStepStore';
 
 function RecapEnd() {
+  const router = useRouter();
+  const { step } = useStepstore();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (step === 9) router.push('/recap');
+    }, 2800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <RecapEndContainer>
       <TitleLine title="offizz" />
+      <Image id="recap-img" src={ic_recap} alt="리캡 캐릭터 이미지" />
+      <div id="recap-end-text">{`이번 워케이션도 수고 많았어요!`}</div>
+      <div id="recap-guide-msg">잠시 후 리캡 페이지로 자동으로 이동합니다</div>
     </RecapEndContainer>
   );
 }
